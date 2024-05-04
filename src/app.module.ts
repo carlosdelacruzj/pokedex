2b4f14c972f4126e896b8bf98fef988f9f2eddda
carlosdelacruzj <delacruzcarlos1405@gmail.com>
2024-05-04 02:24:02 -0500

    fin seccion 7

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,17 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-    MongooseModule.forRoot('mongodb+srv://admin123:admin123@cluster0.lbhwxwy.mongodb.net/'),
+    MongooseModule.forRoot('mongodb+srv://admin123:admin123@cluster0.lbhwxwy.mongodb.net/nest-pokemon'),
     // MongooseModule.forRoot(process.env.MONGODB_URI),
     PokemonModule,
+    CommonModule,
   ],
 })
 export class AppModule {}
--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,12 @@
-export class CreatePokemonDto {}
+import { IsInt, IsPositive, IsString, Min, MinLength } from 'class-validator';
+
+export class CreatePokemonDto {
+  @IsInt({ message: 'El valor debe ser un número entero.' })
+  @IsPositive({ message: 'El número debe ser positivo.' })
+  @Min(1, { message: 'El valor mínimo permitido es 1.' })
+  no: number;
+
+  @IsString({ message: 'El contenido debe ser textual.' })
+  @MinLength(5, { message: 'El nombre debe contener al menos 5 caracteres.' })
+  name: string;
+}
