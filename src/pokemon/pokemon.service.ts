d49ef6b82b0bbf85bc9068380c99820a7be8cef2
carlosdelacruzj <delacruzcarlos1405@gmail.com>
2024-05-17 23:42:58 -0500

    cambio BD

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -84,13 +84,9 @@ export class PokemonService {
   }
 
   async remove(id: string) {
-    // const pokemon = await this.findOne(id);
-    // await pokemon.deleteOne();
-    // const result = await this.pokemonModel.findByIdAndDelete(id);
     const { deletedCount } = await this.pokemonModel.deleteOne({ _id: id });
     if (deletedCount === 0) {
       throw new BadRequestException(`Pokemon with id "${id} not found"`);
-      // throw new HttpException('Pokemon not found', HttpStatus.NOT_FOUND);
     }
     return;
   }
